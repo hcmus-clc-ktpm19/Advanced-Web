d4814a4c60bb09303726cb5215934c47effbdf6f
Nguyen Duc Nam <namworkmc@gmail.com>
2022-11-19 21:13:41 +0700

    Add prettier format fix command

--- sakila-front/vite.config.ts
+++ sakila-front/vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -12,4 +12,4 @@ export default defineConfig({
   server: {
     port: 3000
   }
-})
+});
